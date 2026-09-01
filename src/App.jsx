66156de59a1a3f5e9eeae99293c2a8b470848b24
Beatryz Caroline dos Santos Rodrigues <beatryzsenai2312@gmail.com>
2026-09-01 07:40:14 -0300

    ajustando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,29 @@ import Contato from "./pages/Contato"
 import Erro from "./pages/Erro"
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
+import Filmes from "./pages/Filmes"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Form from "./pages/Form"
-import Filmes from "./pages/Filmes"
 
 //rotas
 export default function App() {
   return (
     <BrowserRouter>
-
+      <div className="flex flex-col min-h-screen">
         <Header />
         <div className="flex flex-1 flex-col">
           <Routes>
-            <Route path="/produtos" element={<Produtos />} />
-            <Route path="/formulario" element={<Form />} />
-            <Route path="/contato" element={<Contato />} />
             <Route path="/" element={<Home />} />
             <Route path='/sobre' element={<Sobre />} />
+            <Route path="/filmes" element={<Filmes />} />
+            <Route path="/locacao" element={<Form />} />
+            <Route path="/contato" element={<Contato />} />
             <Route path="*" element={<Erro />} />
           </Routes>
         </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
